41e964c91271dd43e2786552b0a2050b4a5d0af9
Jerome-Baille <baille.jerome@gmail.com>
2023-12-07 19:43:16 +0100

    Remove unused images and update PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Jerome Baille - Portfolio',
+        short_name: 'JB Portfolio',
+        description: 'Portfolio of Jerome Baille, a french frontend engineer.',
+        theme_color: '#0C2A42',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+    })
+  ],
+  build: {
+    chunkSizeWarningLimit: 700, // size in KB
+  },
 })
